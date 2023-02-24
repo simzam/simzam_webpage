fb7990c5b306eed4ce7dae2c12ab5f07b3ab1893
Simon Iversen <simon.iversen@protonmail.com>
2023-02-24 17:42:52 +0100

    Change from dark to light theme.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,10 @@ Alpine.start()
 import 'bootstrap';
 document.documentElement.setAttribute('data-theme', 'dark');
 
+document.querySelector('[data-switch-dark]').addEventListener('click', function() {
+  document.body.classList.toggle('dark');
+});
+
 // import * as PIXI from 'pixi.js'
 
 
