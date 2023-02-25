fbd981b75f18188e49a8cdf93d8ec6493487ddee
Simon Iversen <simon.iversen@protonmail.com>
2023-02-25 14:50:58 +0100

    Add offcanvas navbar.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,7 @@
-// Comment
-import Alpine from 'alpinejs'
-
 window.htmx = require('htmx.org');
-//import './class-tools';
 
-window.Alpine = Alpine
-Alpine.start()
-
-//import 'bootstrap';
 document.documentElement.setAttribute('data-theme', 'dark');
 
 document.querySelector('[data-switch-dark]').addEventListener('click', function() {
   document.body.classList.toggle('dark');
 });
-
-// import * as PIXI from 'pixi.js'
-
-
-// ENDTE ENDNTE
-/// TEST F
