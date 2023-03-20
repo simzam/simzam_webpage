e7e0c03048e908f152325dfff5f20ffa2712f238
Simon Iversen <simon.iversen@protonmail.com>
2023-03-20 11:23:07 +0100

    Add Bootstrap

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 window.htmx = require('htmx.org');
 
+import "bootstrap/dist/js/bootstrap.bundle";
+
 document.documentElement.setAttribute('data-theme', 'dark');
 
 document.querySelector('[data-switch-dark]').addEventListener('click', function() {
