96d09528bc4d9639ec46a35b0b97300ed96b9bf9
SimZam <simzamdev@protonmail.com>
2023-10-30 19:35:46 +0100

    Setup Wagtail CMS Django project.

---
+++ simzam/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rol4jm&6vd^a2rv#h+)oi0opn!=uz#g5(88_#!oj+zjd0w=oup"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
