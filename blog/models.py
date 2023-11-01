487ec59b3605dde5519071aa136ae647a34266bb
SimZam <simzamdev@protonmail.com>
2023-11-01 17:47:17 +0100

    Start to write models for blog.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+    MultipleChooserPanel,
+)
+from wagtail.images.models import Image
+from modelcluster.fields import ParentalKey
 
-# Create your models here.
+
+class BlogPostPage(Page):
+    # Title of the blog post.
+    blog_post_title = models.CharField(max_length=200, blank=True)
+
+    # Main content of the blog post.
+    body = RichTextField(blank=True)
+
+    # Date when the blog post was published.
+    date_published = models.DateTimeField()
+
+    # Featured image for the blog post.
+
+    content_panels = Page.content_panels + [
+        FieldPanel("title"),
+        FieldPanel("body", classname="full"),
+        FieldPanel("date_published"),
+        MultipleChooserPanel(
+            "gallery_images",
+            label="Gallery images",
+            chooser_field_name="image",
+        ),
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    """
+    Example related image
+    """
+
+    page = ParentalKey(
+        BlogPostPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("caption"),
+    ]
+
+
+class BlogIndexPage(Page):
+    # Introduction for the blog index page.
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+    ]
+
+    subpage_types = ["BlogPostPage", "BlogIndexPage"]
+
+    def get_context(self, request):
+        # Get the most recent blog posts.
+        blog_posts = BlogPostPage.objects.live().order_by("-date_published")
+        return {
+            "blog_posts": blog_posts,
+        }
