0be6d9c1246065b4574d8f57988e841352f09c1d
SimZam <simzamdev@protonmail.com>
2023-11-08 11:01:17 +0100

    Add model for an about page.

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from wagtail.search import index
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
@@ -10,4 +11,50 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("body"),
+    ]
+
+    max_count = 1
+
+
+class AboutPage(Page):
+    # Title field is inherited from Page
+
+    # Add fields specific to the "About" page
+    body = RichTextField(
+        verbose_name="About Text",
+        blank=True,
+    )
+
+    profile_banner = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Profile Banner",
+        help_text="Upload your profile banner (PNG image) here.",
+    )
+
+    profile_photo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Profile Photo",
+        help_text="Upload your profile photo here.",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body", classname="full"),
+        FieldPanel("profile_banner"),
+        FieldPanel("profile_photo"),
+    ]
+
+    parent_page_types = ["home.HomePage"]
+
+    max_count = 1
+
+    search_fields = Page.search_fields + [
+        index.SearchField("body"),
     ]
