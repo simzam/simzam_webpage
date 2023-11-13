82f631d6f5ed0c88f2ebede99b529b0b0b755a1a
SimZam <simzamdev@protonmail.com>
2023-11-13 17:16:11 +0100

    Connect templates to models.

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from wagtail import blocks
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import (
@@ -12,31 +13,45 @@ from wagtail.images.blocks import ImageChooserBlock
 from modelcluster.fields import ParentalKey
 
 
+class ImageBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=False)
+    image = ImageChooserBlock()
+    alt = blocks.CharBlock(require=False)
+
+
 class BlogPostPage(Page):
     # Title of the blog post.
     blog_post_title = models.CharField(max_length=200, blank=True)
 
     # Main content of the blog post.
-    body = RichTextField(blank=True)
-
+    body = StreamField(
+        [
+            ("heading", blocks.CharBlock()),
+            ("image", ImageBlock(required=False)),
+            ("text", blocks.TextBlock(required=False)),
+        ],
+        blank=True,
+        null=True,
+        min_num=2,
+        use_json_field=True,
+    )
     # Date when the blog post was published.
     date_published = models.DateTimeField()
 
     # Featured image for the blog post.
-    images = StreamField(
-        [
-            ("image", ImageChooserBlock()),
-        ],
-        use_json_field=True,
-        blank=True,
-        null=True,
-    )
+    # images = StreamField(
+    #     [
+    #         ("image", ImageChooserBlock()),
+    #     ],
+    #     use_json_field=True,
+    #     blank=True,
+    #     null=True,
+    # )
 
     content_panels = Page.content_panels + [
         FieldPanel("title"),
         FieldPanel("body", classname="full"),
         FieldPanel("date_published"),
-        FieldPanel("images"),
     ]
 
 
