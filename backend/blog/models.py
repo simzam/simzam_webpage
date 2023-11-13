48a4bb34b5ce7aee43dc620d9d39d1eaaf8dbb0b
SimZam <simzamdev@protonmail.com>
2023-11-13 11:26:56 +0100

    Add templates and SCSS.

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
@@ -8,6 +8,7 @@ from wagtail.admin.panels import (
     MultipleChooserPanel,
 )
 from wagtail.images.models import Image
+from wagtail.images.blocks import ImageChooserBlock
 from modelcluster.fields import ParentalKey
 
 
@@ -22,54 +23,20 @@ class BlogPostPage(Page):
     date_published = models.DateTimeField()
 
     # Featured image for the blog post.
+    images = StreamField(
+        [
+            ("image", ImageChooserBlock()),
+        ],
+        use_json_field=True,
+        blank=True,
+        null=True,
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("title"),
         FieldPanel("body", classname="full"),
         FieldPanel("date_published"),
-        MultipleChooserPanel(
-            "gallery_images",
-            label="Gallery images",
-            chooser_field_name="image",
-        ),
-    ]
-
-
-class ReceipePostPage(Page):
-    # Title of the blog post.
-    blog_post_title = models.CharField(max_length=200, blank=True)
-
-    # Main content of the blog post.
-    body = RichTextField(blank=True)
-
-    # Date when the blog post was published.
-    date_published = models.DateTimeField()
-
-    # Featured image for the blog post.
-
-    content_panels = Page.content_panels + [
-        FieldPanel("title"),
-        FieldPanel("body", classname="full"),
-        FieldPanel("date_published"),
-    ]
-
-
-class BlogPageGalleryImage(Orderable):
-    """
-    Example related image
-    """
-
-    page = ParentalKey(
-        BlogPostPage, on_delete=models.CASCADE, related_name="gallery_images"
-    )
-    image = models.ForeignKey(
-        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
-    )
-    caption = models.CharField(blank=True, max_length=250)
-
-    panels = [
-        FieldPanel("image"),
-        FieldPanel("caption"),
+        FieldPanel("images"),
     ]
 
 
