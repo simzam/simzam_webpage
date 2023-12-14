16342156e2892b29da6eb2fccc2a21101dbbc63a
SimZam <simzamdev@protonmail.com>
2023-12-14 20:26:35 +0100

    Use docker compose to run a separate container for frontend and for backend.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,12 +16,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
           'css-loader',
+          // Compiles Sass to CSS
           'sass-loader',
-        ]
+        ],
       }
     ]
   },
