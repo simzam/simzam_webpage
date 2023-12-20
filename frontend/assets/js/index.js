9e32edb3b5ddb652f409d3178dc067f338c67469
SimZam <simzamdev@protonmail.com>
2023-12-20 12:29:43 +0100

    Start to shape templates and styling.

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,36 +1,45 @@
-//import bootstrap from 'bootstrap';
-// Import the htmx library (make sure it's included in your HTML)
-// import '../style/main.scss'
+function rotateOnClick() {
+    const button = document.querySelector('.v-button');
+    // const svg = button.querySelector('svg');
 
-// import 'htmx.org';
+    // Toggle a class to rotate the button by 180 degrees
+    button.classList.toggle('rotate180');
+}
+// Function to generate a random color in hexadecimal format
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-// window.htmx = require('htmx.org');
-// Function to handle the response and update the page content
-// function handleResponse(target, content) {
-//   target.innerHTML = content;
-// }
+// Function to change the background color of the root element to a random color with a smooth transition
+function changeBackgroundColor() {
+    const rootElement = document.documentElement;
+    const newColor = getRandomColor();
+    const currentColor = getComputedStyle(rootElement).getPropertyValue('--background-color') || '#ffffff';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   // Get the button element
-//   const loadButton = document.getElementById('loadDataButton');
+    // Set the new color as a CSS variable for smooth transition
+    rootElement.style.setProperty('--background-color', newColor);
 
-//   // Add an event listener to the button to trigger the htmx request
-//   loadButton.addEventListener('click', (event) => {
-//     // Prevent the default button click behavior
-//     event.preventDefault();
+    // Wait for a moment to allow the CSS variable to be applied
+    setTimeout(() => {
+        rootElement.style.backgroundColor = newColor;
+    }, 0);
+}
 
-//     // Use htmx to make a GET request to a URL and update a target element
-//     htmx.ajax({
-//       url: 'https://jsonplaceholder.typicode.com/posts/1',
-//       target: '#output', // The element where the response will be placed
-//       method: 'get',
-//       onSwap: handleResponse, // Custom function to handle the response
-//     });
-//   });
-// });
-document.documentElement.setAttribute('data-theme', 'dark');
+// Function to change the background color at random intervals
+function changeColorAtRandomInterval() {
+    changeBackgroundColor();
 
-document.querySelector('[data-switch-dark]').addEventListener('click', function() {
-  document.body.classList.toggle('dark');
-});
+    // Generate a random interval between 1 and 3 seconds (1000-3000 milliseconds)
+    const randomInterval = Math.floor(Math.random() * 2000) + 1000;
 
+    // Schedule the next color change
+    setTimeout(changeColorAtRandomInterval, randomInterval);
+}
+
+// Start the color-changing loop
+changeColorAtRandomInterval();
